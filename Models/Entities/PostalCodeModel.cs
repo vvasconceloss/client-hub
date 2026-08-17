510f1ae3d3c5f8d34a3a8e1fe1093f15e0c59a44
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-17 21:34:35 +0100

    chore(models): change the name of the ‘PostalCode’ entity

--- Models/Entities/PostalCodeModel.cs
+++ Models/Entities/PostalCodeModel.cs
@@ -1,11 +1,11 @@
 namespace ClientHub.Models.Entities
 {
-  public class PostalCodeModel
+  public class PostalCode
   {
     public int Id { get; set; }
     public required string Code { get; set; }
     public required string City { get; set; }
 
-    public ICollection<ClientModel> Clients { get; set; } = [];
+    public ICollection<Client> Clients { get; set; } = [];
   }
 }
