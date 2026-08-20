4dcd6f371b58b29864e5d74790b36e04c586c858
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-20 15:10:13 +0100

    feat: apply database migrations on startup in development

--- Program.cs
+++ Program.cs
@@ -12,6 +12,13 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
