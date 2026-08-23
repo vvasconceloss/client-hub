65d1e5f7bf1315176717da2ea3b3dcf06833d5c2
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-23 11:42:31 +0100

    feat(controllers): show the dashboard as the home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,40 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using ClientHub.Models;
+using ClientHub.Models.ViewModels;
+using ClientHub.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace ClientHub.Controllers;
-
-public class HomeController : Controller
+namespace ClientHub.Controllers
 {
-    public IActionResult Index()
+  public class HomeController : Controller
+  {
+    private readonly IClientService _clientService;
+
+    public HomeController(IClientService clientService)
     {
-        return View();
+      _clientService = clientService;
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Index()
+    {
+      var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+      var model = await _clientService.GetDashboardAsync(userId);
+
+      return View(model);
     }
 
     public IActionResult Privacy()
     {
-        return View();
+      return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+  }
 }
