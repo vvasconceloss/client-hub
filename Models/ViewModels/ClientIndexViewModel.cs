b6af8f474418a6e7adb0e29080dd1110b674d203
vvasconceloss <vvasconcelos.dev@gmail.com>
2026-08-23 11:40:25 +0100

    feat(viewmodels): add filtering and sorting state to the client index

--- Models/ViewModels/ClientIndexViewModel.cs
+++ Models/ViewModels/ClientIndexViewModel.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace ClientHub.Models.ViewModels
 {
   public class ClientIndexViewModel
   {
     public List<ClientListItemViewModel> Clients { get; set; } = [];
     public string? Search { get; set; }
+    public int? PostalCodeId { get; set; }
+    public string? SortBy { get; set; }
+    public string? SortDir { get; set; }
+    public List<SelectListItem> PostalCodes { get; set; } = [];
     public int Page { get; set; }
     public int TotalPages { get; set; }
     public bool HasPreviousPage { get; set; }
